47f02a3c205d8cc68262f617204bc6a966bc6f4e
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-20 18:38:03 +0530

    shouldComponentUpdate

---
+++ src/Update01.js
@@ -0,0 +1,29 @@
+// import Members from "./Members";
+import React from "react";
+//import User from "./User";
+class Update01 extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            count:0
+        }
+    }
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate",this.state.count);
+        if(this.state.count>5 && this.state.count<10)
+        {
+            return true;
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="Update01">
+                <h1> Should Component Update {this.state.count}</h1>
+                <button onClick={()=>{this.setState({count:this.state.count+1})}}>Update counter</button>
+            </div>
+        )
+    }
+}
+export default Update01;
