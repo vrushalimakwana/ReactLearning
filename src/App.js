b5e72f4ff205f778ff9bc901ed71e3f4d0f199c3
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-20 15:20:48 +0530

    constructor and render life cycle method

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import Members from "./Members";
+import React from "react";
+import User from "./User";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Render Method in React</h1>
+            <User />
+        </div>
+    )
+}
+
+export default App;
