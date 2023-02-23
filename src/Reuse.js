94794774a8b905a35cb36a463da84b66b085294f
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-23 15:23:35 +0530

    reuse component

---
+++ src/Reuse.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import User from './User'
+function Reuse() {
+    const users = [
+        {
+            name: 'Anil', email: 'anil@test.com', contact: "111"
+        },
+        {
+            name: 'Burce', email: 'bruce@test.com', contact: "222"
+        },
+        {
+            name: 'Peter', email: 'peter@test.com', contact: "333"
+        },
+        {
+            name: 'Sam', email: 'sam@test.com', contact: "444"
+        },
+    ]
+    return (
+        <div className="App">
+            <h1>Reuse Component with List</h1>
+
+            {
+                users.map((item)=><User  data={item} />)
+            }
+        </div>
+    );
+}
+
+export default Reuse;
