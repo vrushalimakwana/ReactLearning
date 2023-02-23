42a84400c9f8b8e52ca03eaeb4c3f1d0f8ee27dd
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-23 14:14:01 +0530

    Nested-list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
 import React from "react";
-import User from "./User";
-class App extends React.Component{
-    constructor()
-    {
-        super();
-        this.state={
-            name:"Vrushali"
-        }
-    }
-    componentDidMount()
-    {
-        console.log("componentDidMount")
-    }
+import {Table} from "react-bootstrap";
+function App() {
+    const users = [
+        { name: 'Pina', email: 'pina@test.com', address: [
+                {Hn:"101",city:'Surat',Country:'India'},
+                {Hn:"304",city:'Vadodara',Country:'India'},
+                {Hn:"602",city:'Rajkot',Country:'India'},
+                {Hn:"102",city:'Vapi',Country:'India'},
+            ]},
+        {
+            name: 'John', email: 'john@test.com', address: [
+                {Hn:"101",city:'Surat',Country:'India'},
+                {Hn:"304",city:'Vadodara',Country:'India'},
+                {Hn:"602",city:'Rajkot',Country:'India'},
+                {Hn:"102",city:'Vapi',Country:'India'},
+            ]},
+        { name: 'Jenny', email: 'jenny@test.com', address: [
+                {Hn:"101",city:'Surat',Country:'India'},
+                {Hn:"304",city:'Vadodara',Country:'India'},
+                {Hn:"602",city:'Rajkot',Country:'India'},
+                {Hn:"102",city:'Vapi',Country:'India'},
+            ]},
+        { name: 'Siri', email: 'siri@test.com', address: [
+                {Hn:"101",city:'Surat',Country:'India'},
+                {Hn:"304",city:'Vadodara',Country:'India'},
+                {Hn:"602",city:'Rajkot',Country:'India'},
+                {Hn:"102",city:'Vapi',Country:'India'},
+            ]},
+    ]
+    return (
+        <div className="App">
+            <center>
+            <h1>List With Bootstrap Table</h1>
+                <Table variant="dark" striped>
+                    <tbody>
+                    <tr>
+                        <td>S.N</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Address</td>
+                    </tr>
 
-    render()
-    {
-        console.log("render")
-        return (
-            <div className="App">
-                <h1>Component DId Mount {this.state.name}</h1>
-                <button onClick={()=>{this.setState({name:"Makwana"})}}>Update Name</button>
-            </div>
-        )
-    }
+                    {
+                        users.map((item,i)=>
+                            <tr key={i}>
+                                <td>{i+1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>
+                                    <Table variant="dark">
+                                        <tbody>
+                                        {
+                                            item.address.map((data)=>
+                                            <tr>
+                                                <td>{data.Hn}</td>
+                                                <td>{data.city}</td>
+                                                <td>{data.country}</td>
+                                            </tr>
+                                            )
+                                        }
+                                        </tbody>
+                                    </Table>
+                                </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </Table>
+
+            </center>
+        </div>
+    );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-//import Form from "./Form";
-//import Profile from "./Profile";
-import Login from "./Login";
-
-ReactDOM.render(
-    //<Form/>,
-    //<Profile/>,
-    <Login/>,
+import App from "./App";
+import ReactDom from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from "./reportWebVitals";
+ReactDom.render(
+    <React.StrictMode>
+    <App />,
+    </React.StrictMode>,
     document.getElementById('root')
 );
+
+reportWebVitals();
