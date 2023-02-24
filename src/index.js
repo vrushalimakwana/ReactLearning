05c2abde9d3b339e17e5850779a144752426c467
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-24 18:12:03 +0530

    useMemo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import App from "./App";
-import ReactDom from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDom.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
     <React.StrictMode>
-        <App />,
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-
