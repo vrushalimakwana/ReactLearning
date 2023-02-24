919c710eaf0d6204e86280bd700a37323593e655
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-24 18:11:28 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,83 +1,22 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import React, { useState } from 'react'
 function App() {
-    const users = [
-        {
-            name: 'Anil', email: 'anil@test.com', address: [
-                { hm: '101', city: 'Noida', country: 'India' },
-                { hm: '10', city: 'Gurgaon', country: 'India' },
-                { hm: '23', city: 'Noida', country: 'India' },
-                { hm: '45', city: 'Delhi', country: 'India' },
-            ]
-        },
-        {
-            name: 'Burce', email: 'bruce@test.com', address: [
-                { hm: '101', city: 'Noida', country: 'India' },
-                { hm: '10', city: 'Gurgaon', country: 'India' },
-                { hm: '23', city: 'Noida', country: 'India' },
-                { hm: '45', city: 'Delhi', country: 'India' },
-            ]
-        },
-        {
-            name: 'Peter', email: 'peter@test.com', address: [
-                { hm: '101', city: 'Noida', country: 'India' },
-                { hm: '10', city: 'Gurgaon', country: 'India' },
-                { hm: '23', city: 'Noida', country: 'India' },
-                { hm: '45', city: 'Delhi', country: 'India' },
-            ]
-        },
-        {
-            name: 'Sam', email: 'sam@test.com', address: [
-                { hm: '101', city: 'Noida', country: 'India' },
-                { hm: '10', city: 'Gurgaon', country: 'India' },
-                { hm: '23', city: 'Noida', country: 'India' },
-                { hm: '45', city: 'Delhi', country: 'India' },
-            ]
-        },
-    ]
+    const [count, setData] = useState(1)
+    const [item, setItem] = useState(20)
+
+    const newApple=React.useMemo(
+        function appleTime() {
+            console.warn("Hello")
+            return 100 * count;
+        }
+        ,[item])
     return (
         <div className="App">
-            <h1>List with Nested Array</h1>
-            <Table variant="dark"  >
-                <tbody>
-                <tr>
-                    <td>S.N</td>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Address</td>
-                </tr>
-                {
-                    users.map((item,i)=>
-                        <tr key={i}>
-                            <td>{i+1}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>
-                                {/*  */}
-                                <Table variant="dark"  >
-                                    <tbody>
-                                    {
-                                        item.address.map((data)=>
-
-                                            <tr>
-                                                <td>{data.hm}</td>
-                                                <td>{data.city}</td>
-                                                <td>{data.country}</td>
-                                            </tr>
-                                        )
-                                    }
-                                    </tbody>
-                                </Table>
-                                {/*  */}
-                            </td>
-                        </tr>
-                    )
-                }
-                </tbody>
-            </Table>
+            <h1>Hooks in React {count}</h1>
+            {newApple}
+            <button onClick={() => setData(count + 1)}>Update State</button>
+            <button onClick={() => setItem(item * 10)}>Update State</button>
 
         </div>
     );
 }
-
 export default App;
