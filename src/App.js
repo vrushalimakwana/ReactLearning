b964d1fe25b182ef285b14b7ec9222e8ed5f6b49
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-02-25 14:17:31 +0530

    controlled component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { useState } from 'react'
+import React, {useState} from "react";
 function App() {
-    const [count, setData] = useState(1)
-    const [item, setItem] = useState(20)
+    const [val,setVal]=useState()
 
-    const newApple=React.useMemo(
-        function appleTime() {
-            console.warn("Hello")
-            return 100 * count;
-        }
-        ,[item])
     return (
         <div className="App">
-            <h1>Hooks in React {count}</h1>
-            {newApple}
-            <button onClick={() => setData(count + 1)}>Update State</button>
-            <button onClick={() => setItem(item * 10)}>Update State</button>
-
+            <h1>Controlled Component</h1>
+            <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
+            <h1>{val}</h1>
         </div>
-    );
+    )
 }
 export default App;
